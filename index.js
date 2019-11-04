a4e03b767166e77fd8f583aa635286dc9c5b77e9
dogwood008 <dogwood008+github@gmail.com>
2019-11-04 21:47:28 +0900

    :sparkles: Use stream API

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ Array.prototype.mean = function() {
 const fs = require('fs');
 const moment = require('moment-timezone');
 const Context = require('@oanda/v20/context').Context;
+
+const Db = require('./db');
+
 const pp = v => console.dir(v, { depth: null, colors: true });
 
 
@@ -63,10 +66,10 @@ const streamingContext = ((host, port, ssl, application, token) => {
   const ctx = new Context(host, port, ssl, application);
   ctx.setToken(token);
   return ctx;
-})(STREAM_DOMAIN, 443, true, 'OANDA', ACCESS_TOKEN);
+});
 
 // stream(accountID, queryParams, streamChunkHandler, responseHandler)
-const streamArgs = ((account_id, instruments, snapshot, heartbeat) => {
+const streamArgs = ((account_id, instruments, snapshot, heartbeat, db) => {
   // snapshot: https://developer.oanda.com/rest-live-v20/best-practices/
   const streamChunkHandler = (msg) => {
     if (msg.type === 'HEARTBEAT' && heartbeat) { console.debug(msg.toString()); return; }
@@ -75,10 +78,16 @@ const streamArgs = ((account_id, instruments, snapshot, heartbeat) => {
       bids: msg.bids.map(p => parseFloat(p.price)) };
     pair.ask = pair.asks.mean();
     pair.bid = pair.bids.mean();
+    const entry = {
+      timestamp: moment().tz('Asia/Tokyo').format(),
+      ask: pair.ask,
+      bid: pair.bid,
+      mid: (0.0 + pair.ask + pair.bid) / 2.0
+    }
+    db.insert(entry)
     console.debug(pair);
   };
   const responseHandler = (resp) => {
-    debugger;
     if (parseInt(msg.statusCode) >= 500) {
       throw `Server Error: ${msg}`;
     }
@@ -90,7 +99,37 @@ const streamArgs = ((account_id, instruments, snapshot, heartbeat) => {
     (message) => streamChunkHandler(message),
     (response) => responseHandler(response)
   ];
-})(ACCOUNT_ID, 'USD_JPY', false, true);
+});
 
-streamingContext.pricing.stream(...streamArgs);
+const closeDb = (db) => {
+  if (typeof db !== 'undefined') {
+    console.error('Close DB.');
+    db.close();
+  }
+};
 
+const setFinallyActions = (db) => {
+  for(let signal of ['SIGHUP', 'SIGINT', 'SIGTERM']) {
+    process.on(signal, () => {
+      console.log(signal);
+      console.log("Caught interrupt signal");
+      closeDb(db);
+      process.exit();
+    });
+  }
+};
+
+const main = () => {
+  const instruments = 'USD_JPY';
+  const sc = streamingContext(STREAM_DOMAIN, 443, true, 'OANDA', ACCESS_TOKEN);
+  const db = new Db('./db.sqlite3', instruments);
+  const args = streamArgs(ACCOUNT_ID, instruments, false, true, db);
+  try {
+    setFinallyActions(db);
+    sc.pricing.stream(...args);
+  } catch {
+    closeDb(db);
+  }
+}
+
+main();
